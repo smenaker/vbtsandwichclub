61d75eaf2665968829f3ef771c817bd0531f4f16
solnovus <solnovus@e99e0b54-7f4f-0410-8836-23da17caf6b6>
2008-06-18 18:45:27 +0000

    massive refactoring, refactored all gui admin sidebar stuff to a separate function, refactored 3 instances of displaying user history to a separate function which kills the concurrency issue with the use of the RecentRequestHandler. This also deprecates the RecentRequestHandler, which I have removed.

--- trunk/vbsandwich.py
+++ trunk/vbsandwich.py
@@ -30,26 +30,10 @@ class Item(db.Model):
 
 class MainPage(webapp.RequestHandler):
     """Main Page View"""
-
     def get(self):
-        user = users.get_current_user()
-        admin = False
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-            if users.is_current_user_admin():
-                admin = True
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-        'url': url,
-        'url_linktext': url_linktext,
-        'admin': admin,
-        }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
+        self.response.out.write(template.render(path, PrepTemplate(self)))
+
 class CreateUser(webapp.RequestHandler):
     def post(self):
         username = self.request.get('username')
@@ -58,12 +42,7 @@ class CreateUser(webapp.RequestHandler):
         for user in match:
             usermatch = user
         if usermatch:
-            template_values = {
-            'message':'Username %s already exists'%username,
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__),'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/userexists')
             return
         else:
             newuser = User(username=username,
@@ -73,72 +52,25 @@ class CreateUser(webapp.RequestHandler):
             newuser.put()
             template_values = {
             'user':newuser,
-            'admin':True if users.is_current_user_admin() else False
             }
             path = os.path.join(os.path.dirname(__file__),'edituser.html')
-            self.response.out.write(template.render(path,template_values))
-
-class Recent(webapp.RequestHandler):
-    def get(self):
-        transactions = Transaction.gql("ORDER BY date DESC LIMIT 1")
-        recentuser=''
-        for transaction in transactions:
-            recentuser = transaction.buyer
-        recentuser_fetch = User.gql("WHERE username=:1 LIMIT 1",recentuser)
-        recentuser_model = None
-        for usermodel in recentuser_fetch:
-            recentuser_model = usermodel
-        
-        recentuser_transactions = Transaction.gql("WHERE buyer=:1 ORDER BY date DESC",recentuser_model.username)
-        template_values = {
-        'username':recentuser_model.username,
-        'balance':recentuser_model.monies,
-        'transactions':recentuser_transactions,
-        'admin':True if users.is_current_user_admin() else False
-        }
-        path = os.path.join(os.path.dirname(__file__), 'history.html')
-        self.response.out.write(template.render(path, template_values))
+            self.response.out.write(template.render(path,PrepTemplate(self,template_values)))
 
 class History(webapp.RequestHandler):
 
     def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'getuserhistory.html')
-        template_values = {
-        'admin':True if users.is_current_user_admin() else False
-        }
-        self.response.out.write(template.render(path, template_values))
+        path = os.path.join(os.path.dirname(__file__), 'getuserhistory.html') 
+        self.response.out.write(template.render(path, PrepTemplate(self)))
     def post(self):
         username = self.request.get('username')
         password = self.request.get('password')
         matchingusers = User.gql("WHERE username=:1",username)
         if matchingusers.count() == 0:
-            template_values = {
-            'message':'Username not found',
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/usernotexists') 
         elif password != matchingusers[0].password:
-            template_values = {
-            'message':'Incorrect password',
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/password') 
         else:
-            self.GetUserHistory(matchingusers[0])
-    def GetUserHistory(self,user):
-        transactions = Transaction.gql("WHERE buyer=:1 ORDER BY date DESC",user.username)
-
-        template_values = {
-        'username':user.username,
-        'balance':user.monies,
-        'transactions':transactions,
-        'admin':True if users.is_current_user_admin() else False
-        }
-        path = os.path.join(os.path.dirname(__file__), 'history.html')
-        self.response.out.write(template.render(path, template_values))
-
+            DisplayUserHistory(self,matchingusers[0])
 
 class Pay(webapp.RequestHandler):
 
@@ -149,28 +81,13 @@ class Pay(webapp.RequestHandler):
             matchingusers = User.gql("WHERE username=:1",username)
             password = self.request.get('password')
             if payment < 0:
-                template_values = {
-                'message':'Please enter a non-negative transaction',
-                'admin':True if users.is_current_user_admin() else False
-                }
-                path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-                self.response.out.write(template.render(path,template_values))
+                self.redirect('error/negative')
                 return
             elif matchingusers.count() == 0:
-                template_values = {
-                'message':'Username not found',
-                'admin':True if users.is_current_user_admin() else False
-                }
-                path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-                self.response.out.write(template.render(path,template_values))
+                self.redirect('error/usernotexists')
                 return
             elif password != matchingusers[0].password:
-                template_values = {
-                'message':'Incorrect password',
-                'admin':True if users.is_current_user_admin() else False
-                }
-                path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-                self.response.out.write(template.render(path,template_values))
+                self.redirect('error/password')
                 return
             newtransaction = Transaction(buyer=username,other=payment,total=-payment)
             if payment:
@@ -178,15 +95,9 @@ class Pay(webapp.RequestHandler):
                 for user in matchingusers:
                     user.monies -= payment
                     user.put()
-            self.redirect('/recent')
-            #History.GetUserHistory(History(),matchingusers[0])
+            DisplayUserHistory(self,matchingusers[0])
         except ValueError:
-            template_values = {
-            'message':'Please enter a floating point value in the amount fields.',
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/float')
 
 class ManageUsers(webapp.RequestHandler):
     """Find a user to edit"""
@@ -195,11 +106,10 @@ class ManageUsers(webapp.RequestHandler):
         if current_user and users.is_current_user_admin():
             userquery = User.all().order('username')
             template_values = {
-            'users':userquery,
-            'admin':True if users.is_current_user_admin() else False
+            'users':userquery
             }
             path = os.path.join(os.path.dirname(__file__), 'manageusers.html')
-            self.response.out.write(template.render(path,template_values))
+            self.response.out.write(template.render(path,PrepTemplate(self,template_values)))
         else:
             self.redirect('/')
     def post(self):
@@ -211,17 +121,11 @@ class ManageUsers(webapp.RequestHandler):
         if usermatch:
             template_values = {
             'user':usermatch,
-            'admin':True if users.is_current_user_admin() else False
             }
             path = os.path.join(os.path.dirname(__file__),'edituser.html')
-            self.response.out.write(template.render(path,template_values))
+            self.response.out.write(template.render(path,PrepTemplate(self,template_values)))
         else:
-            template_values = {
-            'message':'Username %s does not exist yet'%username,
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__),'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/usernotexists')
 
 class Deposit(webapp.RequestHandler):
     def post(self):
@@ -232,26 +136,16 @@ class Deposit(webapp.RequestHandler):
             try:
                 deposit = float(self.request.get('addamount'))
             except ValueError:
-                template_values = {
-                'message':'Please enter a floating point value',
-                'admin':True if users.is_current_user_admin() else False
-                }
-                path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-                self.response.out.write(template.render(path,template_values))
+                self.redirect('error/float')
                 return
             if deposit < 0:
-                template_values = {
-                'message':'Please enter a non-negative value',
-                'admin':True if users.is_current_user_admin() else False
-                }
-                path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-                self.response.out.write(template.render(path,template_values))
+                self.redirect('error/negative')
                 return
             firstmatch.monies += deposit
             firstmatch.put()
             newdeposit = Transaction(buyer=username,total=deposit)
             newdeposit.put()
-            self.redirect('/recent')
+            DisplayUserHistory(self,firstmatch)
             return
         else:
             self.redirect('/')
@@ -285,12 +179,7 @@ class EditUser(webapp.RequestHandler):
                     self.redirect('/manageusers')
                     return
                 except ValueError:
-                    template_values = {
-                    'message':'Please enter a floating point value in the amount fields.',
-                    'admin':True if users.is_current_user_admin() else False
-                    }
-                    path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-                    self.response.out.write(template.render(path,template_values))
+                    self.redirect('error/float')
                     return
         else:
             self.redirect('/')
@@ -300,32 +189,14 @@ class ViewUserHistory(webapp.RequestHandler):
         username = self.request.get('username')
         matchingusers = User.gql("WHERE username=:1",username)
         if matchingusers.count() == 0:
-            template_values = {
-            'message':'Username not found',
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__), 'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/usernotexists')
         else:
-            self.GetUserHistory(matchingusers[0])
-    def GetUserHistory(self,user):
-        transactions = Transaction.gql("WHERE buyer=:1 ORDER BY date DESC",user.username)
+            DisplayUserHistory(self,matchingusers[0])
 
-        template_values = {
-        'username':user.username,
-        'balance':user.monies,
-        'transactions':transactions,
-        'admin':True if users.is_current_user_admin() else False
-        }
-        path = os.path.join(os.path.dirname(__file__), 'history.html')
-        self.response.out.write(template.render(path, template_values))
 class ChangePassword(webapp.RequestHandler):
-    def get(self):
-        template_values = {
-        'admin':True if users.is_current_user_admin() else False
-        }
+    def get(self): 
         path = os.path.join(os.path.dirname(__file__),'changepassword.html')
-        self.response.out.write(template.render(path,template_values))
+        self.response.out.write(template.render(path,PrepTemplate(self)))
     def post(self):
         username = self.request.get('username')
         oldpassword = self.request.get('oldpassword')
@@ -334,49 +205,76 @@ class ChangePassword(webapp.RequestHandler):
 
         matchingusers = User.gql("WHERE username=:1 LIMIT 1",username)
         if matchingusers.count() == 0:
-            template_values = {
-            'message':'Username not found',
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__),'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/usernotexists')
             return
         user = None
         for u in matchingusers:
             user = u
         if oldpassword != user.password:
-            template_values = {
-            'message':'Incorrect password',
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__),'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/password')
             return
         if newpassword1 != newpassword2:
-            template_values = {
-            'message':'New passwords do not match',
-            'admin':True if users.is_current_user_admin() else False
-            }
-            path = os.path.join(os.path.dirname(__file__),'submit_error.html')
-            self.response.out.write(template.render(path,template_values))
+            self.redirect('error/newpassword')
             return
         user.password = newpassword1
         user.put()
         template_values = {
-        'message':'Password changed',
-        'admin':True if users.is_current_user_admin() else False
+        'message':'Password changed'
         }
         path = os.path.join(os.path.dirname(__file__),'submit_success.html')
-        self.response.out.write(template.render(path,template_values))
+        self.response.out.write(template.render(path,PrepTemplate(self,template_values)))
 
 class About(webapp.RequestHandler):
     def get(self):
-        template_values = {
-        'admin':True if users.is_current_user_admin() else False
-        }
         path = os.path.join(os.path.dirname(__file__),'about.html')
-        self.response.out.write(template.render(path,template_values))
+        self.response.out.write(template.render(path,PrepTemplate(self)))
+class Error(webapp.RequestHandler):
+    def get(self,error):
+        if error == 'password':
+            message = 'Incorrect password'
+        elif error == 'float':
+            message = 'Only floating point values accepted'
+        elif error == 'negative':
+            message = 'Only non-negative values accepted'
+        elif error == 'newpassword':
+            message = 'New passwords do not match'
+        elif error == 'usernotexists':
+            message = 'This username does not exist yet'
+        elif error == 'userexists':
+            message = 'This username already exists'
+        else:
+            message = 'UNDEFINED ERROR. POSSIBLY RELATED TO SMOOTH JAZZ.'
+        template_values = {
+        'message':message
+        }
+        path = os.path.join(os.path.dirname(__file__),'submit_error.html')
+        self.response.out.write(template.render(path,PrepTemplate(self,template_values)))
 
+def PrepTemplate(request_handler,template_values={}):
+    user = users.get_current_user()
+    admin = False
+    if users.get_current_user():
+        url = users.create_logout_url(request_handler.request.uri)
+        url_linktext = 'Logout'
+        if users.is_current_user_admin():
+            admin = True
+    else:
+        url = users.create_login_url(request_handler.request.uri)
+        url_linktext = 'Login'
+    template_values['url'] = url
+    template_values['url_linktext'] = url_linktext
+    template_values['admin'] = admin
+    return template_values
+
+def DisplayUserHistory(request_handler,user):
+    transactions = Transaction.gql("WHERE buyer=:1 ORDER BY date DESC",user.username)
+    template_values = {
+    'username':user.username,
+    'balance':user.monies,
+    'transactions':transactions
+    }
+    path = os.path.join(os.path.dirname(__file__), 'history.html')
+    request_handler.response.out.write(template.render(path, PrepTemplate(request_handler,template_values)))
 
 def main():
     application = webapp.WSGIApplication([('/', MainPage),
@@ -385,8 +283,8 @@ def main():
                                         ('/createuser',CreateUser),
                                         ('/viewuserhistory',ViewUserHistory),
                                         ('/changepassword',ChangePassword),
+                                        (r'/error/(.*)',Error),
                                         ('/history', History),
-                                        ('/recent', Recent),
                                         ('/deposit',Deposit),
                                         ('/manageusers',ManageUsers),
                                         ('/edituser',EditUser)],
